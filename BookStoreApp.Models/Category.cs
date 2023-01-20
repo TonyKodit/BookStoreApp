9e49eef85415bfef7bc237872454e680abb81520
Nonso <Onwulinonso@gmail.com>
2023-01-21 00:20:53 +0100

    Add project files.

---
+++ BookStoreApp.Models/Category.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace BookStore.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string? Name { get; set; }
+
+        [Display(Name = "Display Order")]
+        [Range(1,100, ErrorMessage ="Display order must be between 1 and 100 only!")]
+        [Required]
+        public int DisplayOrder { get; set; }
+
+        [Display(Name = "Time Created")]
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
---
+++ BookStoreApp.Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStor.Utility
+{
+    public static class SD
+    {
+        // A place to store all the constants of our application.
+    }
+}
