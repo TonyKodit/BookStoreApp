5b55cc8c7ec7b64c742e70cc7ba776bb021a7cad
Nonso <Onwulinonso@gmail.com>
2023-02-05 11:22:56 +0100

    Details UI

--- BookStoreApp/Areas/Customer/Controllers/HomeController.cs
+++ BookStoreApp/Areas/Customer/Controllers/HomeController.cs
@@ -20,8 +20,18 @@ namespace BookStoreApp.Areas.Customer.Controllers
 
         public IActionResult Index()
         {
-            IEnumerable<Product> prouctList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
-            return View(prouctList);
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,CoverType");
+            return View(productList);
+        }
+        public IActionResult Details(int id)
+        {
+            ShoppingCart cartObj = new()
+            {
+                Count = 1,
+                Product = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id, includeProperties: "Category,CoverType"),
+            };
+
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
